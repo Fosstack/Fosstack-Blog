f17a3cb2f11c738e3b3c9c96fbd65a16d64cdd47
Mohit <mohitsolanki619@gmail.com>
2018-01-12 22:20:03 +0530

    Add app core_blog

---
+++ core_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoreBlogConfig(AppConfig):
+    name = 'core_blog'
---
+++ core_blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    description = models.TextField(max_length=250, blank=True, null=True)
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, default=1,
+        on_delete=models.SET_DEFAULT
+        )
+
+    def __str__(self):
+        return self.title
