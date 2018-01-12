1fb546cffe77c209b88a359cbe984d983217479b
Mohit <mohitsolanki619@gmail.com>
2018-01-13 02:58:40 +0530

    Add about view

--- core_blog/urls.py
+++ core_blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.HomeView.as_view(), name='home'),
-    re_path(r'^create/$', views.CreatePost.as_view(), name='create_post')
+    re_path(r'^create/$', views.CreatePost.as_view(), name='create_post'),
+    re_path(r'^about/$', views.AboutView.as_view(), name='about')
 ]
