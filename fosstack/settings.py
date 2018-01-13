34e9ae7bda0ea95280ea3384d6bf4b2ad3764858
Mohit <mohitsolanki619@gmail.com>
2018-01-13 22:53:31 +0530

    modified settings.py

--- fosstack/settings.py
+++ fosstack/settings.py
@@ -124,13 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR , "core_blog/static")
-]
-
-
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, "../static_cdn")
 MEDIA_ROOT = os.path.join(BASE_DIR, "../media_cdn")
 
