948ee5c93d793a7981bb4414a6cf0e75ef8834ad
Karan Suthar <karansthr@hotmail.com>
2018-01-14 02:00:45 +0530

    added delete with modal

--- core_blog/urls.py
+++ core_blog/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     re_path(r'^$', views.ListPostView.as_view(), name='list_post'),
     re_path(r'^create/$', views.CreatePost.as_view(), name='create_post'),
     re_path(r'^about/$', views.AboutView.as_view(), name='about'),
-    path(
-        '<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'
-    ),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('<slug:slug>/edit', views.PostUpdateView.as_view(), name='post_update'),
+    path('<slug:slug>/delete', views.PostDeleteView, name='post_delete'),
+
 ]
