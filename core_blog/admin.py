2182ec5e849ac6602f913ba4981032c526d999f1
Karan Suthar <karansthr97@gmail.com>
2018-09-03 23:06:48 +0530

    django admin -> fosstack admin

--- core_blog/admin.py
+++ core_blog/admin.py
@@ -3,5 +3,7 @@ from mptt.admin import MPTTModelAdmin
 
 from . import models
 
+admin.site.site_header = 'Fosstack administration'
+
 admin.site.register(models.Post)
 admin.site.register(models.Category, MPTTModelAdmin)
